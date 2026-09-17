1cfe22008d10cc67aff4770addc335c4f9730a28
Leila Peraza <00141525@uca.edu.sv>
2026-09-16 22:39:03 -0600

    Add route to get incidencia by ID

--- routes/incidencias.js
+++ routes/incidencias.js
@@ -9,5 +9,9 @@ router.post('/incidencias', incidenciasController.registrarIncidencia);
 //cuando llegue una peticion GET a incidencias, se ejecuta la funcion listarIncidencias del controlador
 router.get('/incidencias',incidenciasController.listarIncidencias);
 
+// cuando llegue una peticion GET a /incidencias/:id
+// se ejecuta la funcion buscarIncidencia del controlador
+router.get('/incidencias/:id', incidenciasController.buscarIncidencia);
+
 //se hace lo mismo se exporta para para que app.js lo pueda usar
 module.exports = router;
